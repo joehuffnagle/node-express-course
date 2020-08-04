3838e0dbfe356497f8f7474f53ec73d775c1e11f
Joe Huffnagle <jhuffnagle@rmmb-4043.lan>
2020-08-04 13:09:02 -0400

    add dynamic GET route

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
 app.listen(8000,function(){git 
     console.log("server is running")
     })
